67f5486f060c7b6aa15d32b5e0c97ab94965fcce
Tom <2689304953@qq.com>
2019-11-19 10:58:06 +0800

    second commit!!!

---
+++ java-1/src/com/bdnq/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bdnq.test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//输出信息并打印到控制台
+		System.out.println("Hello World");
+		/*
+		System.out.println("Hello World");
+		System.out.println("你好");
+		*/
+	}
+}
